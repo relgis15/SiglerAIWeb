27e472a5c429df242831ea07b720ee6471e491ef
relgis15 <corey.sigler@icloud.com>
2025-06-08 19:04:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ChatBot from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ChatBot />
+  </React.StrictMode>
+);
